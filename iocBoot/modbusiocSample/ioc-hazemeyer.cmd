60315f62178c8d8cba5c7c839fa57de1abe0feef
Francesco Galletti <francesco.galletti@lnf.infn.it>
2025-07-16 09:26:59 +0000

    Add some read records

--- iocBoot/modbusiocSample/ioc-hazemeyer.cmd
+++ iocBoot/modbusiocSample/ioc-hazemeyer.cmd
@@ -8,12 +8,15 @@ hz_registerRecordDeviceDriver(pdbbase)
 
 
 # TCP Connection - Hezemeyer:
-drvAsynIPPortConfigure("Hazemeyer_Asyn", "192.168.0.107:502", 0, 0, 1)
+drvAsynIPPortConfigure("Hazemeyer_Asyn", "192.168.0.107:502", 0, 0, 1) 
+# https://millenia.cars.aps.anl.gov/software/epics/modbusDoc.html#Overview_of_Modbus
+
 # open a channel used by Asyn
 
 # Modbus configuration
 # modbusInterposeConfig(portName, linkType, timeoutMsec, writeDelayMsec)
 modbusInterposeConfig("Hazemeyer_Asyn", 0, 0, 0)
+# https://millenia.cars.aps.anl.gov/software/epics/modbusDoc.html#Overview_of_Modbus
 
 ## Modbus Function Codes Documentation
 # Access         Function Description           Function Code
@@ -29,14 +32,18 @@ modbusInterposeConfig("Hazemeyer_Asyn", 0, 0, 0)
 
 
 ## Write holding registers
-drvModbusAsynConfigure("Hazemeyer_001_WAO", "Hazemeyer_Asyn", 1, 6, 0, 16, 0, 1000, "HAZEMEYER")
+# drvModbusAsynConfigure("Hazemeyer_001_WAO", "Hazemeyer_Asyn", 1, 6, 0, 16, 0, 1000, "HAZEMEYER")
 
 ## Read holding registers
-drvModbusAsynConfigure("Hazemeyer_001_RAO", "Hazemeyer_Asyn", 1, 3, 0, 46, 0, 1000, "HAZEMEYER")
+drvModbusAsynConfigure("Hazemeyer_001_RAO", "Hazemeyer_Asyn", 1, 3, 0, 37, 0, 1000, "HAZEMEYER")
+# drvModbusAsynConfigure("Hazemeyer_001_Rbit", "Hazemeyer_Asyn",  1, 1, 0, 16, 0, 1000, "HAZEMEYER")
+# https://millenia.cars.aps.anl.gov/software/epics/modbusDoc.html#Overview_of_Modbus
+
+
 
 
 # Load database
-dbLoadRecords("../../db/hz.db", "P=PS:LAB:,R=PS1:,PORT=Hazemeyer_001_RAO, WPORT=Hazemeyer_001_WAO ,TIMEOUT=1000")
+dbLoadRecords("../../db/hz.db", "P=PS:LAB:,R=PS1:,PORT=Hazemeyer_001_RAO, WPORT=Hazemeyer_001_WAO , PORTbit=Hazemeyer_001_Rbit,TIMEOUT=1000")
 
 # Oppure usa il file di sostituzione
 # dbLoadTemplate("db/PowerSupply.substitutions")
@@ -46,8 +53,11 @@ iocInit
 
 
 # Enable Hazemeyer_Asyn (TCP/IP port) trace
-# asynSetTraceMask("Hazemeyer_Asyn", 0, 0x9)
-# asynSetTraceIOMask("Hazemeyer_Asyn", 0, 0x2)  
+
+
+# asynSetTraceIOMask  "Hazemeyer_001_RAO", 0, 4 
+# asynSetTraceMask    "Hazemeyer_001_RAO", 0, 9
+
 
 
 
