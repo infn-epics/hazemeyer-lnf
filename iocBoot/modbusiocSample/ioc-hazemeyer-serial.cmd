25df0ade498d417c117ef43b9663e9792dfff3b2
riesposi <riccardo.esposito@lnf.infn.it>
2025-07-25 13:42:37 +0000

    modified .cmd and hz.db

--- iocBoot/modbusiocSample/ioc-hazemeyer-serial.cmd
+++ iocBoot/modbusiocSample/ioc-hazemeyer-serial.cmd
@@ -40,15 +40,18 @@ dbLoadRecords("../../db/hz.db", "P=SPARC:MAG,R=AC1SOL01,PORT=HAZMEYER_AC1SOL01_P
 
 
 
-## Write holding registers slave 8
-# drvModbusAsynConfigure("HAZMEYER_AC1SOL02_PORT_WAO", "Hazemeyer_Asyn", 8,    16, 4, 4, 0, 1000, "HAZEMEYER")
+# Write holding registers slave 8
+drvModbusAsynConfigure("HAZMEYER_AC1SOL02_PORT_WAO", "Hazemeyer_Asyn", 8,    16, 4, 4, 0, 1000, "HAZEMEYER")
 
-## Read holding registers
-# drvModbusAsynConfigure("HAZMEYER_AC1SOL02_PORT_RAO", "Hazemeyer_Asyn", 8,    3, 4, 12, 0, 1000, "HAZEMEYER")
+# Read holding registers
+drvModbusAsynConfigure("HAZMEYER_AC1SOL02_PORT_RAO", "Hazemeyer_Asyn", 8,    3, 4, 12, 0, 1000, "HAZEMEYER")
 
-# Load database
-# dbLoadRecords("../../db/hz.db", "P=SPARC:MAG,R=AC1SOL02,PORT=HAZMEYER_AC1SOL02_PORT_RAO, WPORT=HAZMEYER_AC1SOL02_PORT_WAO,IMAX=200,VMAX=110 ,TIMEOUT=2000")
+Load database
+dbLoadRecords("../../db/hz.db", "P=SPARC:MAG,R=AC1SOL02,PORT=HAZMEYER_AC1SOL02_PORT_RAO, WPORT=HAZMEYER_AC1SOL02_PORT_WAO,IMAX=200,VMAX=110 ,TIMEOUT=2000")
 
+
+Load database
+dbLoadRecords("../../db/unimag.db", "P=SPARC:MAG,R=AC1SOL02")
 # Oppure usa il file di sostituzione
 # dbLoadTemplate("db/PowerSupply.substitutions")
 
